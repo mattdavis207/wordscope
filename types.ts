9f2e5d8af5a855ec98fe592146b9211bcf12983b
Matthew Davis <matthewdavis207@gmail.com>
2025-07-15 00:24:44 -0400

    Updated types

--- types.ts
+++ types.ts
@@ -8,18 +8,3 @@ export type DefinitionData = {
     pronunciationAudio?: string
     extrasFetched?: boolean
   }
-
-
-  declare module "*.png" {
-    const value: string
-    export default value
-  }
-  declare module "*.jpg" {
-    const value: string
-    export default value
-  }
-  declare module "*.svg" {
-    const value: string
-    export default value
-  }
-  
